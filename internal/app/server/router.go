64722468c55bafdb6b1acdc6516256681cf6acf4
vijayaedke <707viee@gmail.com>
2023-10-22 14:30:13 +0530

    add api timeout

--- internal/app/server/router.go
+++ internal/app/server/router.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"context"
+	"net/http"
+	"time"
 	"url-shortner/internal/app/handler"
 	"url-shortner/internal/config"
 
@@ -13,6 +16,8 @@ func NewAppRoutes(config config.Configuration, handler handler.BaseHandler) *gin
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
 		c.Next()
 	})
+	route.Use(apiTimeoutMiddleware())
+
 	route.UseRawPath = true
 	route.UnescapePathValues = false
 
@@ -23,4 +28,16 @@ func NewAppRoutes(config config.Configuration, handler handler.BaseHandler) *gin
 	URLShortApiGroup.GET("/", handler.Redirect)
 
 	return route
+}
+
+func apiTimeoutMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		duration := time.Duration(20 * time.Second)
+		ct, cancel := context.WithDeadline(ctx.Request.Context(), time.Now().Add(duration))
+		defer cancel()
+		ctx.Next()
+		if ct.Err() != nil {
+			ctx.JSON(int(http.DefaultClient.Timeout), "Request Timed Out")
+		}
+	}
 }
